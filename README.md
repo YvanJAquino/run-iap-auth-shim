e75d20fa2dc10828b4370e11949f76e48d876839
Yvan J. Aquino <yaquino@google.com>
2022-07-27 14:44:03 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# run-iap-auth-shim
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
+	parent := context.Background()
+	logger := log.Default()
+	router := &Router{
+		logger: logger,
+	}
+	mux := http.DefaultServeMux
+	mux.Handle("/", router)
+	server := &http.Server{
+		Addr:        addr,
+		Handler:     mux,
+		BaseContext: func(net.Listener) context.Context { return parent },
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
+
+type Router struct {
+	logger *log.Logger
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	targetURL := "https://hello-a67fdjzmma-uc.a.run.app"
+	http.Redirect(w, req, targetURL, http.StatusPermanentRedirect)
+}
